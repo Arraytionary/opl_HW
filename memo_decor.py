a1dabc4df5540c6bf382cad91d32b6d72ec0cc9b
Arraytionary <kmkanokpon3@gmail.com>
2018-09-25 00:03:19 +0700

    done memo_decor

--- memo_decor.py
+++ memo_decor.py
@@ -1,5 +1,5 @@
 # @memoized
-def memorize_fib(n):
+def memoize_fib(n):
     mem = {}
     def memo_fib(x):
         if x not in mem: 
@@ -13,4 +13,23 @@ def fib(n):
     else:
         return fib(n-1) + fib(n-2)
 
-lib = memorize_fib(fib)
+fib = memoize_fib(fib)
+
+def memoize_ncr(n):
+    mem = {}
+    def memo_ncr(x, r):
+        if (x,r) not in mem: 
+            mem[(x,r)] = n(x,r)    
+        return mem[(x,r)]
+    return memo_ncr
+
+
+def n_choose_r(n, r):
+    if r is 0:
+        return 1
+    elif n is r:
+        return 1
+    else:
+        return n_choose_r(n - 1, r - 1) + n_choose_r(n - 1, r)
+
+n_choose_r = memoize_ncr(n_choose_r)
